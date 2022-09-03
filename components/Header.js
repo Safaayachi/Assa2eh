f7d3c5a3429fab6c69c6fa2716a4c543577589be
safa ayachi <safa.ayachi@polytechnicien.tn>
2022-09-03 22:03:48 +0100

    svg

--- components/Header.js
+++ components/Header.js
@@ -3,13 +3,15 @@ import Image from 'next/image'
 
 const Header = () => {
   return (
-    <div className='flex w-full h-4/5'>
-        <div className='w-1/2 h-full '>
-            
+    <div className='flex-row md:flex w-full h-4/5'>
+        <div className='w-full h-1/2 text-center md:w-1/2 md:h-full '>
+        <h1 className='text-5xl inline-block text-navyBlue font-bold md:hidden'><span className='text-gold  '>الاختلاف</span> نخدمكم<br/> باحتراف </h1>
         </div>
-        <div className='flex-row  w-1/2 h-full bg-paleGray text-9xl inline-block text-navyBlue text-center font-bold'>
+        <div className='w-full h-1/2 flex-row  md:w-1/2  md:h-full  bg-paleGray lg:text-8xl md:text-6xl  text-center'>
            
-           <h1><span className='text-gold '>الاختلاف</span> نخدمكم<br/> باحتراف </h1>
+           <h1 className='hidden  md:inline-block md:text-navyBlue md:font-bold '><span className='text-gold  '>الاختلاف</span> نخدمكم<br/> باحتراف </h1>
+           
+           
            
         </div>
         
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 export default function Home() {
   return (
-    <div className="h-screen">
+    <div className="h-screen w-screen">
       <Navbar  />
       <Header />
     </div>
