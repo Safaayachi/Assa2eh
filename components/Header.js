967b5a713733235d1fe98b5cacb63ecfa9484ad3
safa ayachi <safa.ayachi@polytechnicien.tn>
2022-09-04 20:57:00 +0100

    navBar

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,61 @@
 import React from "react";
+import { Popover } from "@headlessui/react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="flex-row md:flex w-full h-4/5">
-      <div className="w-full h-1/2 text-center md:w-1/2 md:h-full ">
-        <h1 className="text-5xl inline-block text-navyBlue font-bold md:hidden">
-          <span className="text-gold  ">الاختلاف</span> نخدمكم
-          <br /> باحتراف{" "}
-        </h1>
-      </div>
-      <div className="w-full h-1/2 flex-row  md:w-1/2  md:h-full  bg-paleGray lg:text-8xl md:text-6xl  text-center">
-        <h1 className="hidden  md:inline-block md:text-navyBlue md:font-bold ">
-          <span className="text-gold  ">الاختلاف</span> نخدمكم
-          <br /> باحتراف{" "}
-        </h1>
-      </div>
-    </header>
+    <>
+      <nav className=" flex justify-between items-center w-full px-16 md:shadow-lg h-24 ">
+        <div className="hidden justify-between space-x-14 md:flex">
+          <button className="w-40 h-12 bg-gold text-white text-xl font-bold leading-loose">
+            تسجيل الدخول
+          </button>
+        </div>
+
+        <ul className="hidden md:flex items-center space-x-16 h-20 text-xl font-bold text-navyBlue">
+          <li className="flex h-24 border-b-transparent items-center justify-center active:text-gold active:border-b-4 active:border-gold  hover:text-gold ">
+            <Link href="/">اتصل بنا</Link>
+          </li>
+          <li className="flex h-24 border-b-transparent items-center justify-center active:text-gold active:border-b-4 active:border-gold  hover:text-gold 	">
+            <Link href="/">الفنادق</Link>
+          </li>
+          <li className="flex h-24 border-b-transparent items-center justify-center active:text-gold active:border-b-4 active:border-b-gold  hover:text-gold ">
+            <Link href="/">حجز نقل</Link>
+          </li>
+          <li className="flex h-24 border-b-transparent items-center justify-center active:text-gold active:border-b-4 border-gold  hover:text-gold ">
+            <Link href="/">حجز فندق</Link>
+          </li>
+        </ul>
+
+        <Popover className="md:hidden">
+          {({ open }) => (
+            <>
+              <Popover.Button>
+                <i
+                  className={`icon-menu_black_24dp1 text-red-500 ${
+                    open ? "hidden" : "block"
+                  }`}
+                />
+              </Popover.Button>
+
+              <Popover.Panel className=" absolute bg-white w-64 h-full "></Popover.Panel>
+            </>
+          )}
+        </Popover>
+        <div>
+          <Link href="/">
+            <a>
+              <div className="relative w-40 h-20">
+                <Image src="/images/logo.svg" layout="fill" alt="logo" />
+              </div>
+            </a>
+          </Link>
+        </div>
+      </nav>
+
+      <header className="flex-row md:flex w-full h-4/5"></header>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import Navbar from "../components/Navbar";
+
 import Header from "../components/Header";
 export default function Home() {
   return (
     <div className="">
-      <Navbar  />
       <Header />
     </div>
   );
