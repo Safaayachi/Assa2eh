6c36477fb1a325a396279b1aa3c6ab60dfda3982
safa ayachi <safa.ayachi@polytechnicien.tn>
2022-09-04 20:38:36 +0100

    navBar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,24 +4,24 @@ import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <div className=" flex justify-between items-center w-full px-16 md:shadow-lg h-16 ">
+    <div className=" flex justify-between items-center w-full px-16 md:shadow-lg h-24 ">
       <div className="hidden justify-between space-x-14 md:flex">
         <button className="w-40 h-12 bg-gold text-white text-xl font-bold leading-loose">
           تسجيل الدخول
         </button>
       </div>
       
-        <ul className="hidden md:flex items-center space-x-16 h-16 text-xl font-bold text-navyBlue">
-          <li className="flex h-16 border-b-transparent items-center justify-center active:text-gold active:border-b-4 active:border-gold  hover:text-gold ">
+        <ul className="hidden md:flex items-center space-x-16 h-20 text-xl font-bold text-navyBlue">
+          <li className="flex h-24 border-b-transparent items-center justify-center active:text-gold active:border-b-4 active:border-gold  hover:text-gold ">
             <Link href="/">اتصل بنا</Link>
           </li>
-          <li className="flex h-16 border-b-transparent items-center justify-center active:text-gold active:border-b-4 active:border-gold  hover:text-gold 	">
+          <li className="flex h-24 border-b-transparent items-center justify-center active:text-gold active:border-b-4 active:border-gold  hover:text-gold 	">
             <Link href="/">الفنادق</Link>
           </li>
-          <li className="flex h-16 border-b-transparent items-center justify-center active:text-gold active:border-b-4 active:border-b-gold  hover:text-gold ">
+          <li className="flex h-24 border-b-transparent items-center justify-center active:text-gold active:border-b-4 active:border-b-gold  hover:text-gold ">
             <Link href="/">حجز نقل</Link>
           </li>
-          <li className="flex h-16 border-b-transparent items-center justify-center active:text-gold active:border-b-4 border-gold  hover:text-gold ">
+          <li className="flex h-24 border-b-transparent items-center justify-center active:text-gold active:border-b-4 border-gold  hover:text-gold ">
             <Link href="/">حجز فندق</Link>
           </li>
         </ul>
@@ -45,12 +45,12 @@ export const Navbar = () => {
       <div>
         <Link href="/">
           <a>
-            <div className="relative w-28 h-12">
+            <div className="relative w-60 h-20">
               <Image
                 src="/images/logo.svg"
                 layout="fill"
                 objectFit="cover"
-                alt="dhfhskj"
+                alt="logo"
               />
             </div>
           </a>
