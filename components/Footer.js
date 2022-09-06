fa6c9c50099b389b6a2377fa3b94c6504e9e41ad
safa ayachi <safa.ayachi@polytechnicien.tn>
2022-09-06 20:00:52 +0100

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,15 +5,16 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="flex-col">
-      <div className="relative h-72 w-full md:hidden">
+      <div className="relative h-72 w-full md:h-96">
         <Image
           src="/images/footerCover.jpg"
           alt="hero-cover"
           layout="fill"
           objectFit="cover"
         />
-        <div className="absolute w-full h-72 bg-secondary opacity-60"></div>
-          <div className="absolute p-14  flex-col justify-center items-center space-y-8">
+        <div className="absolute w-full h-72 bg-secondary opacity-60 md:w-1/2 md:h-96"></div>
+        
+          <div className="absolute p-14  flex-col justify-center items-center  space-y-8 md:py-24 md:px-36">
               <div>
                 <h1 className="text-2xl text-tint font-bold text-center">
                   اشترك فى قائمتنا البريدية
@@ -24,11 +25,11 @@ const Footer = () => {
               </div>
               <div>
                 <form className="flex space-x-2 justify-center">
-                  <button className="h-12 w-1/3 bg-tint font-bold" type="submit">
+                  <button className="h-12 w-1/3 bg-tint font-bold md:h-16" type="submit">
                     اشترك
                   </button>
                   <input
-                    className="bg-transparent border w-3/4 border-tint text-center p-2 focus:outline-none"
+                    className="bg-transparent border h-12 w-3/4 border-tint text-center p-2 focus:outline-none md:w-96 md:h-16"
                     type="email"
                     name="mailbox"
                     placeholder="البريد الالكتروني"
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Search from "../components/Search";
 export default function Home() {
   return (
     <div className="">
       <Header />
+      <Search/>
       <Footer/>
     </div>
   );
