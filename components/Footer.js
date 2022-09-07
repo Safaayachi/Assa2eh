68de3f61ebe4fe682ea0e2ae94f36c7a3bee6d59
safa ayachi <safa.ayachi@polytechnicien.tn>
2022-09-07 04:26:27 +0100

    signup

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import signUp from "../pages/signUp";
 
 const Footer = () => {
   return (
@@ -84,7 +85,7 @@ const Footer = () => {
                 <ul className=" md:flex space-x-6 ">
                   <Link href="/">المدونة</Link>
                   <Link href="/">ضمان الخدمة</Link>
-                  <Link href="/">حول</Link>
+                  <Link href="/signUp">حول</Link>
                 </ul>
               </div>
 
